6d95852ad88727d5002387dfc113e374bfeb240f
timdavidcole <timdavidcole@gmail.com>
2019-10-31 17:48:14 +0000

    added Add function

--- BST.js
+++ BST.js
@@ -13,6 +13,28 @@ export default class BST {
     let node = this.root;
     if (node === null) {
       this.root = new Node(data);
+      return null;
     }
+    const addNode = data => {
+      if (data <= node.data) {
+        if (node.left === null) {
+          node.left = new Node(data);
+          return null;
+        } else {
+          node = node.left;
+          addNode(data);
+        }
+      }
+      if (data > node.data) {
+        if (node.right === null) {
+            node.right = new Node(data);
+            return null;
+          } else {
+            node = node.right;
+            addNode(data);
+          }
+      }
+    };
+    addNode(data);
   }
 }
--- __tests__/bstTests.spec.js
+++ __tests__/bstTests.spec.js
@@ -34,5 +34,50 @@ describe("BST", () => {
       bst.add(4);
       expect(bst.getRoot().data).toEqual(4);
     });
+    test("should add new node to left of root if data is less than root", () => {
+      const bst = new BST();
+      bst.add(4);
+      bst.add(2);
+      expect(bst.getRoot().left.data).toEqual(2);
+    });
+
+    test("should add new node to right of root if data is less than root", () => {
+      const bst = new BST();
+      bst.add(4);
+      bst.add(6);
+      expect(bst.getRoot().right.data).toEqual(6);
+    });
+
+    test("should add new node twice to the left if lower than previous nodes", () => {
+      const bst = new BST();
+      bst.add(4);
+      bst.add(3);
+      bst.add(2);
+      expect(bst.getRoot().left.left.data).toEqual(2);
+    });
+
+    test("should add new node twice to the right if lower than previous nodes", () => {
+      const bst = new BST();
+      bst.add(4);
+      bst.add(5);
+      bst.add(6);
+      expect(bst.getRoot().right.right.data).toEqual(6);
+    });
+
+    test("should add new node to the right then left", () => {
+      const bst = new BST();
+      bst.add(4);
+      bst.add(6);
+      bst.add(5);
+      expect(bst.getRoot().right.left.data).toEqual(5);
+    });
+
+    test("should add new node to the left then right", () => {
+      const bst = new BST();
+      bst.add(4);
+      bst.add(2);
+      bst.add(3);
+      expect(bst.getRoot().left.right.data).toEqual(3);
+    });
   });
 });
